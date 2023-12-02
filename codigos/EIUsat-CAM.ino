93482a4e9b4bb68df772965ef93bcb9c53d5436e
gabriel-603 <123210381+gabriel-603@users.noreply.github.com>
2023-12-02 18:46:12 -0300

    Update EIUsat-CAM.ino
    / use webserver pre made sample and alter main file

--- codigos/EIUsat-CAM.ino
+++ codigos/EIUsat-CAM.ino
@@ -1,32 +1,22 @@
+// use webserver pre made sample and alter main file
 
-//Libraries
-#include <esp32cam.h>
-#include "Arduino.h"
-#include "time.h"
+#include "esp_camera.h"
+#include "SD_MMC.h"
 #include <HardwareSerial.h>
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
+#include "camera_pins.h"
 
-
-//Camera defines
-#define CAMERA_MODEL_AI_THINKER
-
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
+const char * photoPrefix = "/photo_";
+int photoNumber = 0;
 
 
 //UART Define
@@ -34,16 +24,10 @@ HardwareSerial SerialPort(2); // use UART2
 
 
 void setup() {
-  
-Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
 
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-
-  //UART setup
-  SerialPort.begin(15200, SERIAL_8N1, 16, 14); 
-  
-  // CAM config
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -64,10 +48,10 @@ Serial.begin(9600);
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
   } else {
@@ -76,42 +60,77 @@ Serial.begin(9600);
     config.fb_count = 1;
   }
 
+  #if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  #endif
 
-  // Init Camera
-  esp_err_t err = esp_camera_init(&config);
+  // camera init
+  esp_err_t err = esp_camera_init( & config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-  
 
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s -> id.PID == OV3660_PID) {
+    s -> set_vflip(s, 1); // flip it back
+    s -> set_brightness(s, 1); // up the brightness just a bit
+    s -> set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s -> set_framesize(s, FRAMESIZE_UXGA);
+
+  #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s -> set_vflip(s, 1);
+  s -> set_hmirror(s, 1);
+  #endif
+
+  Serial.println("Initialising SD card");
+  if (!SD_MMC.begin()) {
+    Serial.println("Failed to initialise SD card!");
+    return;
+  }
+
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("SD card slot appears to be empty!");
+    return;
+  }
 
 }
 
 void loop() {
-  digitalWrite(4, HIGH);
-  camera_fb_t * fb = NULL;  
-
-  // Take Picture with Camera
-  fb = esp_camera_fb_get();  
-  if(!fb) {
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
     Serial.println("Camera capture failed");
     return;
   }
-  
-  String test;
-  Serial.println( fb->len);
-  //test=to_string(fb)
 
+  String photoFileName = photoPrefix + String(photoNumber) + ".jpg";
+  fs::FS & fs = SD_MMC;
+  Serial.printf("Picture file name: %s\n", photoFileName.c_str());
+
+  File file = fs.open(photoFileName.c_str(), FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+  } else {
+    file.write(fb -> buf, fb -> len);
+    Serial.printf("Saved file to path: %s\n", photoFileName.c_str());
+    ++photoNumber;
+  }
+  // Send the data in five chunks spaced by 0.2 seconds
   const char *data = (const char *)fb->buf;
-  SerialPort.print(data);
+  int chunkSize = fb->len / 4; // Divide the data into five chunks
+  for (int i = 0; i < 4; ++i) {
+    SerialPort.write(data + i * chunkSize, chunkSize); // Send each chunk
+    delay(200); // Delay for 0.2 seconds between chunks
+  }
 
-  
+  file.close();
   esp_camera_fb_return(fb);
-  digitalWrite(4, LOW);
-  delay(1500);
-   
 
-
-
+  delay(10000);
 }
